3eb77f1f2b5702ac159723f1904bd37ca2189e96
Foluso Ojo <folusoojo96@gmail.com>
2023-08-30 11:43:20 +0100

    To get a tex displayed in the User Interface using STATE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/");
     const data = await res.json();
-    console.log(data);
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
 
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <p>
+        You have read <strong>{count}</strong> pieces of advice
+      </p>
     </div>
   );
 }
